456a61691428e448cf6f28d35cd3a5773e8fb1ae
Amadou Moctar Diallo <mctrjalloh@gmail.com>
2018-07-24 15:53:44 +0000

    Graphing variable provided by user input

--- plot.py
+++ plot.py
@@ -2,34 +2,52 @@ import dash
 import dash_core_components as dcc 
 import dash_html_components as html 
 
+from dash.dependencies import Input, Output
+
 import pandas as pd
 
 # Load the names of columns
-with open('data/kddcup.names.txt', 'r') as names_file:
-    lines = names_file.readlines()[1:]
-    names = [lines[i].split(':')[0] for i in range(len(lines))]
-names.append('attack_type')
+
 
 # Load the data
-data = pd.read_csv("data/kddcup.data_10_percent_corrected", names=names, nrows=20000)
 # print(len(data))
 # print(data.src_bytes)
+
+with open('data/kddcup.names.txt', 'r') as names_file:
+        lines = names_file.readlines()[1:]
+        names = [lines[i].split(':')[0] for i in range(len(lines))]
+        names.append('attack_type')
+
 
 
 app = dash.Dash()
 
 app.layout = html.Div(children=[
     html.H2("Visualizing Data"),
-    dcc.Graph(id='kdd-graph',
-              figure={
-                  'data': [
-                      {'x': data.index, 'y': data['src_bytes'], 'type': 'line', 'name': 'src_bytes'},
-                  ],
-                  'layout': {
-                      'title': 'kddcup data'
-                  }
-              })
+    dcc.Input(id='kdd_input', type='text', value=''),
+    html.Div(id='kdd_graph'),
 ])
 
+@app.callback(
+    Output(component_id='kdd_graph', component_property='children'),
+    [Input(component_id='kdd_input', component_property='value')]
+)
+def update_graph(column):
+    data = pd.read_csv("data/kddcup.data_10_percent_corrected", names=names, nrows=20000)
+
+    graph = dcc.Graph(id='kdd_graph',
+        figure={
+            'data': [
+                {'x': data.index, 'y': data['attack_type']},
+                {'x': data.index, 'y': data[column]}
+            ], 
+            'layout': {
+                'title': column
+            }
+    })
+
+    return graph
+
+
 if __name__ == '__main__':
-    app.run_server()
+    app.run_server() 
