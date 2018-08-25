5c77a9e7700c91b2d5efa45a468147405a4da66d
Amadou Moctar Diallo <mctrjalloh@gmail.com>
2018-08-25 19:04:36 +0000

    Use a config file to get user arguments

--- kddcup/main.py
+++ kddcup/main.py
@@ -1,39 +1,51 @@
 from kddcup.core.data import KddCupData
 from kddcup.core.model import KddCupModel
 
-from kddcup.core.constants import training_file, testing_file
-
-inputs = []
-layers = [{'neurons': 8, 'activation': 'relu'},
-          {'neurons': 4, 'activation': 'relu'}]
-targets = []
+import json
 
 
 class KddCup(object):
-    def __init__(self):
+    def __init__(self, config_file="./config.json"):
+        with open(config_file) as f:
+            config = json.load(f)
+        self.config = config
+
+    """Train a model on input data and return loss and accuracy"""
+    def train(self):
+        data = KddCupData(filename=self.config["train"]["file"], 
+                          nrows=self.config["train"]["rows"],
+                          batch=self.config["train"]["batch_iter"])
+
+        model = KddCupModel(inputs=self.config["inputs"], 
+                            targets=self.config["targets"], 
+                            layers=self.config["layers"])
+                        
+        return model.train(data=data,
+                           batch_size=self.config["train"]["batch_train"], 
+                           epochs=self.config["train"]["epochs"], 
+                           verbose=self.config["train"]["verbose"])\
+                    .save(path=self.config["train"]["save_model"])\
+                    ['loss', 'accuracy']
+
+    def test(self):
+        data = KddCupData(filename=self.config["test"]["file"],
+                          nrows=self.config["test"]["rows"],
+                          batch=self.config["test"]["batch_iter"])
+        model = KddCupModel(inputs=self.config["inputs"], 
+                            targets=self.config["targets"], 
+                            model_path=self.config["test"]["model_path"])
+        return model.test(data)['loss', 'accuracy']
+
+
+    def evolve(self, ):
         pass
 
-    def train(self, inputs=[], targets=[], training_file=None, model=None, 
-                batch_read=50000, batch_train=128, epochs=15, save_dir=None):
-        pass
-
-    def test(self, inputs=[], targets=[], testing_file=None, model=None):
+    def predict(self, packet):
         pass
 
 
 if __name__ == '__main__':
-    loss, acc = KddCupModel(inputs=inputs, targets=targets)\
-                    .train(KddCupData(training_file, batch=100000), epochs=15)\
-                    .test(KddCupData(testing_file))\
-                    .save('/home/mctrjalloh/.kddcup/ckpts')\
-                    .print()\
-                    ['loss', 'accuracy']
-
-    # loss, acc = KddCupModel(model_path='/home/mctrjalloh/.kddcup/ckpts/normal.-vs-other.model-acc-91.6.h5',
-    #                         inputs=inputs, targets=targets)\
-    #                 .test(KddCupData(filename=testing_file))\
-    #                 .print()\
-    #                 ['loss', 'accuracy']
+    
+    loss, acc = KddCup('kddcup/config.json').test()
     print(loss)
     print(acc)
-    # print(round(loss, 4), round(100*acc, 2))
