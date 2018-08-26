a40abcac6c6a8022dc16c00cda3b588ac5cb9b77
Amadou Moctar Diallo <mctrjalloh@gmail.com>
2018-08-26 15:39:47 +0000

    Finished json model saving

--- kddcup/core/model.py
+++ kddcup/core/model.py
@@ -30,7 +30,6 @@ class Model(object):
         self.loss = -1
         self.accuracy = -2
 
-
     def __getitem__(self, *args):
         if isinstance(*args, str):
             return self.__dict__[str(*args)]
@@ -39,26 +38,29 @@ class Model(object):
 
     @property
     def input_dim(self):
-        return self.data.shape[1] - 1 # not include the output dim
+        return self.data.shape[1] - 1  # not include the output dim
 
     @property
     def output_dim(self):
         return len(set(self.data.attack_types))
 
     def __build_model(self, layers):
-        # layers = [{'neurons': neurons, 'activation': activation, ...}, 
+        # layers = [{'neurons': neurons, 'activation': activation, ...},
                   # {'neurons': neurons, 'activation': activation, ...},
                   #  ... ]
         model = Sequential()
-        model.add(Dense(layers[0]['neurons'], input_shape=(self.input_dim,), activation=layers[0]['activation']))
+        model.add(Dense(layers[0]['neurons'], input_shape=(
+            self.input_dim,), activation=layers[0]['activation']))
         for layer in layers[1:]:
             model.add(Dense(layer['neurons'], activation=layer['activation']))
         model.add(Dense(self.output_dim, activation='softmax'))
-        model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+        model.compile(loss='binary_crossentropy',
+                      optimizer='adam', metrics=['accuracy'])
         return model
 
     def train(self, batch_size=128, epochs=10, verbose=1):
-        self.kmodel.fit(self.data.X, self.data.Y, batch_size=batch_size, epochs=epochs, verbose=verbose)
+        self.kmodel.fit(self.data.X, self.data.Y,
+                        batch_size=batch_size, epochs=epochs, verbose=verbose)
         return self
 
     def test(self, data=None):
@@ -70,11 +72,9 @@ class Model(object):
         return self
 
 
-
-    
 class KddCupModel(object):
-    def __init__(self, inputs=[], targets=[], layers=[{'neurons': 1, 'activation': 'relu'}], 
-                       model_path=None):
+    def __init__(self, inputs=[], targets=[], layers=[{'neurons': 1, 'activation': 'relu'}],
+                 model_path=None):
         """Initialize self.model and set/get inputs and targets
 
         @params inputs: list of input properties to consider; default to [] 
@@ -92,8 +92,7 @@ class KddCupModel(object):
         else:
             self.targets = sorted(kddcup_targets)
         self.layers = layers
-        if model_path:
-            self = self.load(model_path)
+        self.model_path = model_path
         self.loss = -1
         self.accuracy = -2
 
@@ -102,14 +101,16 @@ class KddCupModel(object):
             return self.__dict__[str(*args)]
         keys = list(*args)
         return [self.__dict__[key] for key in keys]
-    
+
     def train(self, data, batch_size=128, epochs=5, verbose=1):
-        model = Model() # emply model with kmodel==None
+        model = Model()  # emply model with kmodel==None
         for d in data:
-            model = Model(d[self.inputs][self.targets], layers=self.layers, kmodel=model.kmodel)
+            model = Model(d[self.inputs][self.targets],
+                          layers=self.layers, kmodel=model.kmodel)
             if model.output_dim == len(self.targets):
                 print("Training..")
-                model.train(batch_size=batch_size, epochs=epochs, verbose=verbose)
+                model.train(batch_size=batch_size,
+                            epochs=epochs, verbose=verbose)
             else:
                 print("Skipping..")
         self.model = model
@@ -117,9 +118,9 @@ class KddCupModel(object):
 
     def test(self, data):
         print("Testing..")
-        l_a = [self.model.test(d[self.inputs][self.targets])['loss', 'accuracy'] for d in data]
+        l_a = [self.model.test(d[self.inputs][self.targets])[
+            'loss', 'accuracy'] for d in data]
         l_a = np.array(l_a)
-        print(l_a)
         loss, acc = l_a[:, 0].tolist(), l_a[:, 1].tolist()
         if len(loss) == 1 and len(acc) == 1:
             loss = loss[0]
@@ -128,30 +129,34 @@ class KddCupModel(object):
         self.accuracy = acc
         return self
 
-    def save(self, path):
+    def save(self, path=None):
+        print('Saving..')
+        loss, acc = round(self.loss, 4), round(100*self.accuracy, 2)
+        to_file = '-vs-'.join(self.targets)+'model-acc-{}.json'.format(acc)
         if path:
-            print('Saving..')
-            loss, acc = round(self.loss, 4), round(100*self.accuracy, 2)
-            to_file = '-vs-'.join(self.targets)+'model-acc-{}.json'.format(acc)
-            data = {"inputs": self.inputs, "targets": self.targets,
-                    "k_model": self.model.kmodel.get_config()}
-            with open(os.path.join(path, to_file), 'w') as f:  
-                json.dump(data, f)
+            self.model_path = os.path.join(path, to_file)
+        data = {"inputs": self.inputs, "targets": self.targets,
+                "k_model": self.model.kmodel.get_config()}
+        with open(self.model_path, 'w') as f:
+            json.dump(data, f, indent=2)
         return self
 
-    def load(self, path):
+    def load(self, path=None):
+        if path:
+            self.model_path = path
         print('Loading..')
-        with open(path) as f:  
+        with open(self.model_path) as f:
             data = json.load(f)
         self.inputs, self.targets = data["inputs"], data["targets"]
         keras_model = Sequential.from_config(data["k_model"])
-        keras_model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+        keras_model.compile(loss='binary_crossentropy',
+                            optimizer='adam', metrics=['accuracy'])
         self.model = Model(kmodel=keras_model)
         return self
 
     def print(self):
         all_inputs, all_targets = '', ''
-        if not self.inputs: 
+        if not self.inputs:
             all_inputs = '(all inputs)'
         if not self.targets:
             all_targets = '(all targets)'
--- kddcup/main.py
+++ kddcup/main.py
@@ -13,52 +13,54 @@ class KddCup(object):
         self.config = config
 
     """Train a model on input data and return loss and accuracy"""
+
     def train(self):
-        data = KddCupData(filename=self.config["train"]["file"], 
-                          nrows=self.config["train"]["rows"],
-                          batch=self.config["train"]["batch_iter"])
-        model = KddCupModel(inputs=self.config["inputs"], 
-                            targets=self.config["targets"], 
-                            layers=self.config["layers"])                      
-        return model.train(data=data,
-                           batch_size=self.config["train"]["batch_train"], 
-                           epochs=self.config["train"]["epochs"], 
+        train_data = KddCupData(filename=self.config["train"]["file"],
+                                nrows=self.config["train"]["rows"],
+                                batch=self.config["train"]["batch_iter"])
+        test_data = KddCupData(filename=self.config["test"]["file"],
+                               nrows=self.config["test"]["rows"],
+                               batch=self.config["test"]["batch_iter"])
+        model = KddCupModel(inputs=self.config["inputs"],
+                            targets=self.config["targets"],
+                            layers=self.config["layers"])
+        return model.train(data=train_data,
+                           batch_size=self.config["train"]["batch_train"],
+                           epochs=self.config["train"]["epochs"],
                            verbose=self.config["train"]["verbose"])\
-                    .test(data)\
-                    .save(path=self.config["train"]["save_model"])\
-                    ['loss', 'accuracy']
+            .test(data=test_data)\
+            .save(path=self.config["train"]["save_model"])['model_path']
 
     def test(self):
         data = KddCupData(filename=self.config["test"]["file"],
                           nrows=self.config["test"]["rows"],
                           batch=self.config["test"]["batch_iter"])
-        model = KddCupModel(inputs=self.config["inputs"], 
-                            targets=self.config["targets"], 
-                            model_path=self.config["test"]["model_path"])
+        model = KddCupModel(
+            model_path=self.config["test"]["model_path"]).load()
         return model.test(data)['loss', 'accuracy']
-
 
     def evolve(self):
         pop = Population(space=self.config["kddcup_properties"][:-1],
-                         targets=self.config["targets"], 
+                         targets=self.config["targets"],
                          brain=self.config["layers"])
         return pop.evolve(train_env=self.config["train"]["file"],
-                        test_env=self.config["test"]["file"],
-                        train_surface=self.config["train"]["rows"],
-                        train_hops=self.config["train"]["batch_iter"],
-                        train_iterations=self.config["train"]["epochs"],
-                        train_verbose=self.config["train"]["verbose"],
-                        test_surface=self.config["test"]["rows"], 
-                        NGEN=self.config["evolve"]["generations"],
-                        MUTPB=self.config["evolve"]["mutation_prob"],
-                        CXPB=self.config["evolve"]["crossover_prob"]
-                        )
+                          test_env=self.config["test"]["file"],
+                          train_surface=self.config["train"]["rows"],
+                          train_hops=self.config["train"]["batch_iter"],
+                          train_iterations=self.config["train"]["epochs"],
+                          train_verbose=self.config["train"]["verbose"],
+                          test_surface=self.config["test"]["rows"],
+                          NGEN=self.config["evolve"]["generations"],
+                          MUTPB=self.config["evolve"]["mutation_prob"],
+                          CXPB=self.config["evolve"]["crossover_prob"]
+                          )
 
     def predict(self, packet):
         pass
 
 
 if __name__ == '__main__':
-    KddCup('kddcup/config.json').train()
+    # model_path = KddCup('kddcup/config.json').train()
     loss, acc = KddCup('kddcup/config.json').test()
     print(loss, acc)
+    # print(model_path)
